3baf307fc0849b4b398e1b5fc0c65215f2f4799d
ar3d <info@ar3d.it>
2022-11-28 17:42:09 +0100

    Update info4.js

--- js/info4.js
+++ js/info4.js
@@ -15,7 +15,7 @@
  */
 'use strict';
 
-scene.hotspotContainer().createHotspot(document.querySelector("#info4"), { yaw: 3.12, pitch: 0.15 });
+scene.hotspotContainer().createHotspot(document.querySelector("#info4"), { yaw: 3, pitch: 0 });
 
 document.querySelector("#info4 .icon_wrapper").addEventListener('click', function() {
   document.querySelector("#info4").classList.toggle('expanded');
